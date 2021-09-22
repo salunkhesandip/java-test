2e49a549fe321ce28c4f38c6d5960944af38e6a5
sandipsalunkhe <sandip.v.salunkhe@gmail.com>
2021-09-22 19:34:08 +0100

    Story 1 : git config

--- TMOCodeTest/src/com/inhouse/apptest/Greeting.java
+++ TMOCodeTest/src/com/inhouse/apptest/Greeting.java
@@ -4,7 +4,17 @@ public class Greeting {
 
 	public static void main(String[] args) {
 	System.out.println("Hello World!");
-
+	int ch = 2;
+	switch(ch) {
+	case 1:
+	case 2:
+		System.out.println("1");
+		break;
+		
+		default :
+			System.out.println("default case");
+	}
+	
 	}
 
 }
